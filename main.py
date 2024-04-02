ce6c483fb35fc489e9f490f8ddfc41486d7bf55b
KOSINEPALLI ARJUN SAI <sreesaiarjun@gmail.com>
2024-04-02 16:50:31 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,38 @@
+import streamlit as st 
+import google.generativeai as genai
+
+
+genai.configure(api_key="AIzaSyAiFezktf4LupPMEMWpwMnDEu7p-WwCITw")
+def ai(txt):
+    
+    for m in genai.list_models():
+        if 'generateContent' in m.supported_generation_methods:
+            print(m.name)
+
+    model_name = ["gemini-1.0-pro-001", "tunedModels/aishbot-4ksyf1p6e1xq"]
+    model = genai.GenerativeModel(model_name[0])
+    response = model.generate_content("You are AishBot, a multimodal AI language model developed by Model Mavericks. I am designed to understand and generate human language, and to answer questions regarding all your academics and provide information to the best of my abilities.How can I help you today?"+txt)
+    return response.text
+
+st.title("AishBot")
+
+command = st.chat_input("Enter your Query...")
+
+if "message" not in st.session_state:
+    st.session_state.message = []
+
+for chat in st.session_state.message:
+    with st.chat_message(chat["role"]):
+        st.write(chat["message"])
+
+if command:
+    with st.chat_message("USER"):
+        st.write(command)
+        st.session_state.message.append({"role":"USER","message":command})
+   
+    with st.chat_message("BOT"):
+        data = ai(command)
+        st.write(data)
+        st.session_state.message.append({"role":"BOT","message":data})
+
+print(st.session_state.message)
