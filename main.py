b5468e870d25d7b58843b706084d358c46a23659
KOSINEPALLI ARJUN SAI <sreesaiarjun@gmail.com>
2024-04-02 17:30:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import streamlit as st 
 import google.generativeai as genai
 
-
+history = []
 genai.configure(api_key="AIzaSyAiFezktf4LupPMEMWpwMnDEu7p-WwCITw")
+init_prompt = "Your name is AishBot, you should act as AishBot, a multimodal AI language model developed by Model Mavericks. It's designed to understand and generate human language, and to answer questions regarding all academics and provide information to the best of it's abilities.Also suggest some courses from IQinternz(Courses offered are Web Development.Python(Basics-Advance)Java(Basics-Advance)Cyber Security(Using Parrot Os And All The Tools.)RPA(Robotic Process Automation)Artificial Intelligence.Data Science.Pharmacovigilance.)"
+
 def ai(txt):
     
     for m in genai.list_models():
@@ -11,10 +13,19 @@ def ai(txt):
 
     model_name = ["gemini-1.0-pro-001", "tunedModels/aishbot-4ksyf1p6e1xq"]
     model = genai.GenerativeModel(model_name[0])
-    model.generate_content(f"You are a chatbot named AishBot, a multimodal AI language model developed by Model Mavericks. I am designed to understand and generate human language, and to answer questions regarding all your academics and provide information to the best of my abilities.Also suggest some courses from IQinternz(Courses offered are)")
-    response = model.generate_content(txt)
-    return response.text
+    
+     # Log the user input
+    history.append("You: " + txt)
+    
+    # Generate response based on user input
+    response_text = model.generate_content("\n".join(history))
+    
+    # Log the model response
+    history.append("AishBot:" + response_text.text)
+    
+    return response_text.text
 
+ai(init_prompt)
 st.title("AishBot")
 
 command = st.chat_input("Enter your Query...")
